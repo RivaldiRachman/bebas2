3473fc7401651161fa74661b1f62239d03c60155
unknown <rivaldidd@gmail.com>
2023-11-23 11:01:48 -0800

    praktikum1

---
+++ src/praktikum1/Latihan.java
@@ -0,0 +1,23 @@
+package praktikum1;
+import java.util.Scanner;
+public class Latihan {
+    public static void main (String args[]) {
+    	 System.out.print("Hello World");
+    	 int angka1, angka2, hasil;
+    	 Scanner input = new Scanner(System.in);
+    	 
+    	 System.out.println("masukkan angka 1 :");
+    	 angka1 = input.nextInt();
+    	 
+    	 System.out.println("masukkan angka 2 :");
+    	 angka2 = input.nextInt();
+    	 
+    	 hasil= angka1 + angka2;
+    	 System.out.println("hasilnya : "+hasil);
+    	 
+    	 String namaDepan, namaBelakang, fullname;
+    	 System.out.print("masukkan nama depan :");
+    	 namaDepan = input.next()
+;    	
+    }
+}
